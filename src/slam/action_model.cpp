f795850706fdb033434cf8e964798f3131d2e49d
eliwg <eliwg@umich.edu>
2023-02-10 11:34:37 -1000

    apply_action and particle resampling

--- src/slam/action_model.cpp
+++ src/slam/action_model.cpp
@@ -5,17 +5,59 @@
 #include <cmath>
 #include <iostream>
 
-
 ActionModel::ActionModel(void)
+: k1_(0.01f)
+, k2_(0.005f)
+, alpha2(0.02f)
+, alpha4(0.02f)
+, initialized_(false)
 {
     //////////////// TODO: Handle any initialization for your ActionModel /////////////////////////
+    //change k1, k2 in my experiment (and alpha2,4)
+    //Prof Du: the parameters would determine how much error it's adding to the RTR action- 
+    //so you can see how increasing or decreasing the param values impact the spread of particles)
+    std::random_device rd;
+    numberGenerator_ = std::mt19937(rd());
 }
 
 
 bool ActionModel::updateAction(const pose_xyt_t& odometry)
 {
     ////////////// TODO: Implement code here to compute a new distribution of the motion of the robot ////////////////
-    return false;
+    if(!initialized_){
+        previousPose_ = odometry;
+        initialized_ = true;
+    }
+
+    float deltaX = odometry.x - previousPose_.x;
+    float deltaY = odometry.y - previousPose_.y;
+    float deltaTheta = angle_diff(odometry.theta, previousPose_.theta);
+    float direction = 1.0; //rotation direction
+
+    //we are implementing the RTR model from lecture
+    trans_ = std::sqrt(deltaX * deltaX + deltaY * deltaY);
+    rot1_ = angle_diff(std::atan2(deltaY, deltaX), previousPose_.theta);
+
+    if(std::abs(rot1_) > M_PI_2){
+        //sanity check if it has rotated over pi/2 --> we went backwards and changed direction
+        rot1_ = angle_diff(M_PI, rot1_);
+        direction = -1.0;
+    }
+
+    rot2_ = angle_diff(deltaTheta, rot1_);
+
+    bool moved = (deltaX!=0) || (deltaY!=0) || (deltaTheta!=0); //has the robot moved?
+    if(moved){
+        rot1Std_ = std::sqrt(k1_ * std::abs(rot1_)); //k1 is std dev scalar for rot1
+        tranStd_ = std::sqrt(k2_ * std::abs(trans_));
+        rot2Std_ = std::sqrt(k1_ * std::abs(rot2_));
+    }
+
+    utime_ = odometry.utime;
+    previousPose_ = odometry;
+    trans_ *= direction;
+
+    return moved;
 }
 
 
@@ -24,5 +66,60 @@ particle_t ActionModel::applyAction(const particle_t& sample)
     ////////////// TODO: Implement your code for sampling new poses from the distribution computed in updateAction //////////////////////
     // Make sure you create a new valid particle_t. Don't forget to set the new time and new parent_pose.
 
-    return sample;
+    particle_t newSample = sample; //= previous sample
+    
+    //this demo: easy and simple one --> normal distribution
+    //we should implement code to sample new poses from the desired distribution as updated above
+    
+    /* FROM HER VIDEO
+    float sampleRot1 = std::normal_distribution<>(rot1_, rot1Std_)(numberGenerator_);
+    float sampleRot2 = std::normal_distribution<>(rot2_, rot2Std_)(numberGenerator_);
+    float sampleTrans = std::normal_distribution<>(trans_, tranStd_)(numberGenerator_);
+
+    newSample.pose.x += sampleTrans * std::cos(sample.pose.theta + sampleRot1);
+    newSample.pose.y += sampleTrans * std::sin(sample.pose.theta + sampleRot1);
+    newSample.pose.theta = wrap_to_pi(sample.pose.theta + sampleRot1 + sampleRot2);
+    newSample.pose.utime = utime_; //updated current time
+    newSample.parent_pose = sample.pose;
+    */
+
+    float x = sample.pose.x;
+    float y = sample.pose.y;
+    float theta = sample.pose.theta;
+
+    // float x_bar = previousPose_.x;
+    // float y_bar = previousPose_.y;
+    // float theta_bar = previousPose_.theta;
+
+    // float x_bar_prime = previousPose_.parent_pose.x;
+    // float y_bar_prime = previousPose_.parent_pose.y;
+    // float theta_bar_prime = previousPose_.parent_pose.theta;
+
+    // float delta_rot_1 = atan2(y_bar_prime - y_bar, x_bar_prime - x_bar) - theta_bar;
+    // float delta_trans = std::sqrt(std::pow(x_bar_prime - x_bar, 2) + std::pow(y_bar_prime - y_bar, 2));
+    // float delta_rot_2 = theta_bar_prime - theta_bar - delta_rot_1;
+
+    //the book: The function sample(b) generates a random sample from a zero-centered
+    //distribution with variance b. It may, for example, be implemented using the algorithms in
+    //Table 5.4.
+    //k1=alpha1, k2 =alpha3, cross correlations = alpha2,alpha4
+    // rot1Std_ = k1_ * delta_rot_1 + alpha2 * delta_trans;
+    // tranStd_ = k2_ * delta_trans + alpha4*(delta_rot_1 + delta_rot_2);
+    // rot2Std_ = k1_*delta_rot_2 + alpha2*delta_trans;
+    
+    float delta_rot_1_hat = rot1_ - std::normal_distribution<float>(0, rot1Std_)(numberGenerator_);
+    float delta_trans_hat = trans_ - std::normal_distribution<float>(0, tranStd_)(numberGenerator_);
+    float delta_rot_2_hat = rot2_ - std::normal_distribution<float>(0, rot2Std_)(numberGenerator_);
+
+    // float x_prime = x + delta_trans_hat * std::cos(theta + delta_rot_1_hat);
+    // float y_prime = y + delta_trans_hat*std::sin(theta + delta_rot_1_hat);
+    // float theta_prime = theta + delta_rot_1_hat + delta_rot_2_hat;
+
+    newSample.pose.x = x + delta_trans_hat * std::cos(theta + delta_rot_1_hat);
+    newSample.pose.y = y + delta_trans_hat*std::sin(theta + delta_rot_1_hat);
+    newSample.pose.theta = theta + delta_rot_1_hat + delta_rot_2_hat;
+    newSample.pose.utime = utime_; //updated current time
+    newSample.parent_pose = sample.pose;
+
+    return newSample; //newly sampled after applying action model (just with a normal distribution)
 }
--- src/slam/particle_filter.cpp
+++ src/slam/particle_filter.cpp
@@ -7,7 +7,7 @@
 ParticleFilter::ParticleFilter(int numParticles)
 : kNumParticles_ (numParticles)
 {
-    assert(kNumParticles_ > 1);
+    assert(kNumParticles_ > 1); //TODO: play with the number of particles --> impacts performance/speed
     posterior_.resize(kNumParticles_);
 }
 
@@ -15,6 +15,18 @@ ParticleFilter::ParticleFilter(int numParticles)
 void ParticleFilter::initializeFilterAtPose(const pose_xyt_t& pose)
 {
     ///////////// TODO: Implement your method for initializing the particles in the particle filter /////////////////
+    double sampleWeight = 1.0/kNumParticles_; //uniformly assign to initialize
+
+    posteriorPose_ = pose;
+
+    for(auto &&p : posterior_){
+        p.pose.x = pose.x;
+        p.pose.y = pose.y;
+        p.pose.theta = pose.theta;
+        p.pose.utime = pose.utime;
+        p.parent_pose = p.pose;
+        p.weight = sampleWeight; // = 1/num particles
+    }
 }
 
 
@@ -78,15 +90,54 @@ std::vector<particle_t> ParticleFilter::resamplePosteriorDistribution(void)
 {
     //////////// TODO: Implement your algorithm for resampling from the posterior distribution ///////////////////
     
-    std::vector<particle_t> prior;
-    return prior;
+    //her demo:
+    std::vector<particle_t> prior = posterior_;
+    // double sampleWeight = 1.0/kNumParticles_;
+    std::random_device rd;
+    std::mt19937 generator(rd());
+    // std::normal_distribution<> dist(0.0, 0.04); //center at 0, 0.04 is standard dev
+
+    //in live demo: resample posterior from a normal distribution; this is incorrect
+    //our implementation: sensor model, low variance sampler from lecture using cumulative sum
+
+    std::vector<particle_t> Xt; //running mean
+    float r = generator() / static_cast<float>(generator.max()); //[0,1] random float 
+    r /= kNumParticles_; //[0, 1/M] where M = kNumParticles_
+    int c = prior[1].weight;
+    int i = 1; //c++ is 0-indexed, not 1-indexed (changed algo from slides)
+    int index = 1; 
+
+    for(auto &p : prior){
+        //her demo:
+        // p.pose.x = posteriorPose_.x + dist(generator); //add a little noise --> demo purpose
+        // p.pose.y = posteriorPose_.y + dist(generator);
+        // p.pose.theta = posteriorPose_.theta + dist(generator);
+        // p.pose.utime = posteriorPose_.utime;
+        // p.parent_pose = posteriorPose_;
+        // p.weight = sampleWeight;
+
+        float U = r + (index - 1)/kNumParticles_;
+        while(U > c){
+            i += 1;
+            c += prior[i].weight;
+        }
+        Xt.push_back(prior[i]);
+    }
+
+    return Xt;
 }
 
 
 std::vector<particle_t> ParticleFilter::computeProposalDistribution(const std::vector<particle_t>& prior)
 {
     //////////// TODO: Implement your algorithm for creating the proposal distribution by sampling from the ActionModel
+    //apply the action for each particle
     std::vector<particle_t> proposal;
+
+    for(auto &p : prior){
+        proposal.push_back(actionModel_.applyAction(p));
+    }
+
     return proposal;
 }
 
@@ -98,6 +149,19 @@ std::vector<particle_t> ParticleFilter::computeNormalizedPosterior(const std::ve
     /////////// TODO: Implement your algorithm for computing the normalized posterior distribution using the 
     ///////////       particles in the proposal distribution
     std::vector<particle_t> posterior;
+    double sumWeights = 0.0; 
+
+    for(auto &&p : proposal){
+        particle_t weighted = p; //weighed particle
+        weighted.weight = sensorModel_.likelihood(weighted,laser,map);
+        sumWeights += weighted.weight;
+        posterior.push_back(weighted);
+    }
+
+    for(auto &&p : posterior_){
+        p.weight /= sumWeights;
+    }
+
     return posterior;
 }
 
@@ -106,5 +170,21 @@ pose_xyt_t ParticleFilter::estimatePosteriorPose(const std::vector<particle_t>& 
 {
     //////// TODO: Implement your method for computing the final pose estimate based on the posterior distribution
     pose_xyt_t pose;
+    double xMean = 0.0;
+    double yMean = 0.0;
+    double cosThetaMean = 0.0;
+    double sinThetaMean = 0.0;
+
+    for(auto &p : posterior){
+        xMean += p.weight * p.pose.x;
+        yMean += p.weight * p.pose.y;
+        cosThetaMean += p.weight * std::cos(p.pose.theta); //NOTE: her code didn't have += and instead had =?
+        sinThetaMean += p.weight * std::sin(p.pose.theta); //NOTE: her code didn't have += and instead had =?
+    }
+
+    pose.x = xMean;
+    pose.y = yMean;
+    pose.theta = std::atan2(sinThetaMean, cosThetaMean);
+
     return pose;
 }
